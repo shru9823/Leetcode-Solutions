2be12ae116124ca87ea9483903c52a946fb7d431
shru9823 <Shrutikohakade9823@gmail.com>
2024-01-30 09:41:54 -0500

    Time: 2 ms (48.17%), Space: 8.2 MB (9.19%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int one=1, two=1, n=s.length();
+        if(s[0] == '0'){return 0;}
+        for(int i=1; i<n;i++){
+            int cur=0, num=stoi(s.substr(i-1, 2));
+            if(s[i] == '0' && (num < 10 || num > 26)){
+                return 0;
+            }
+            if(s[i] != '0' && num <= 26 && num >= 10){
+                cur += one+two;
+            }
+            else if(s[i] == '0'){
+                cur += two;
+            }
+            else{
+                cur += one;
+            }
+            two = one;
+            one = cur;
+        }
+        return one;
+    }
+};
