e4cd856e1fa864c7783fe0e9535795d7ae3b33e6
shru9823 <Shrutikohakade9823@gmail.com>
2024-02-25 10:47:48 -0500

    Time: 0 ms (100.00%), Space: 8.2 MB (31.12%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -5,18 +5,10 @@ public:
         if(s[0] == '0'){return 0;}
         for(int i=1; i<n;i++){
             int cur=0, num=stoi(s.substr(i-1, 2));
-            if(s[i] == '0' && (num < 10 || num > 26)){
-                return 0;
-            }
-            else if(s[i] != '0' && num <= 26 && num >= 10){
-                cur += one+two;
-            }
-            else if(s[i] == '0'){
-                cur += two;
-            }
-            else{
-                cur += one;
-            }
+            if(s[i] == '0' && (num < 10 || num > 26)){return 0;}
+            else if(s[i] != '0' && num <= 26 && num >= 10){cur += one+two;}
+            else if(s[i] == '0'){cur += two;}
+            else{cur += one;}
             two = one;
             one = cur;
         }
