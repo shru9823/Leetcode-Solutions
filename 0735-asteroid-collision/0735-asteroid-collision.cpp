82355dac7da51c15a8b658661c9a271d5655bd87
shru9823 <Shrutikohakade9823@gmail.com>
2024-03-16 14:36:13 -0400

    Time: 7 ms (86.26%), Space: 20.5 MB (49.93%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -5,10 +5,10 @@ public:
         int n=asteroids.size();
         st.push(asteroids[0]);
         for(int i=1;i<n;i++){
-            if(st.empty() || !st.empty() && ((asteroids[i] < 0 && st.top() < 0) || (asteroids[i] > 0 && st.top() > 0) || (asteroids[i] > 0 && st.top() < 0))){
-                st.push(asteroids[i]);continue;
-            }
-            int flag=0;
+            // if(st.empty() || !st.empty() && ((asteroids[i] < 0 && st.top() < 0) || (asteroids[i] > 0 && st.top() > 0) || (asteroids[i] > 0 && st.top() < 0))){
+            //     st.push(asteroids[i]);continue;
+            // }
+            int flag=1;
             while(!st.empty() && ( (asteroids[i] < 0 && st.top() > 0))){
                 int a = abs(asteroids[i]);
                 int b = abs(st.top());
