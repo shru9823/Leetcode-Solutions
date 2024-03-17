800a5b98bddc19666d17b0b3c2ec4a1d97c09598
shru9823 <Shrutikohakade9823@gmail.com>
2024-03-17 10:45:49 -0400

    Time: 3 ms (64.54%), Space: 11.2 MB (42.55%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -4,7 +4,6 @@ public:
         int l=0,r=nums.size()-1, n=nums.size();
         while(l<=r){
             int mid = l+(r-l)/2;
-            cout<<l<<" "<<r<<endl;
             if(nums[mid] > (mid+1<n?nums[mid+1]:INT_MIN) && nums[mid] > (mid-1>=0?nums[mid-1]:INT_MIN)){
                 return mid;      
             }else if(mid+1 < n && nums[mid] < nums[mid+1]){
