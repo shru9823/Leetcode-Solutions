405c75c4c6055a05d379b7f59dfc7603f1f09247
shru9823 <Shrutikohakade9823@gmail.com>
2024-03-26 12:00:44 -0400

    Time: 45 ms (56.62%), Space: 164.7 MB (25.94%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
+    pair<int,int> find(int l, int r, string s){
+        int n=s.length();
+        while(l>=0 && r<n){
+            if(s[l] == s[r]){
+                l--;r++;
+            }else{break;}
+        }
+        return make_pair(l+1, r-1);
+    }
     string longestPalindrome(string s) {
-        int n= s.length(),a=0,b=0;
-        vector<vector<bool>> dp(n+1, vector<bool>(n+1,0));
-        for(int i=1;i<=n;i++){
-            dp[i][i] = 1;
-        }
-        for(int i=1;i<n;i++){
-            if(s[i-1] == s[i]){dp[i][i+1]=1;a=i-1;b=i;}
-        }
-        for(int i=3;i<=n;i++){
-            int k=1;
-            for(int j=i;j<=n;j++){
-                if(s[k-1]==s[j-1]){
-                    if(dp[k+1][j-1]==1){
-                        dp[k][j]=1;
-                        a=k-1;b=j-1;
-                    }
-                }
-                k++;
+        int n=s.length();
+        pair<int,int> p;
+        p = make_pair(0,0);
+        for(int i=0;i<n;i++){
+            auto a = find(i,i, s);
+            if(p.second-p.first < a.second-a.first){
+                p.first = a.first;
+                p.second=a.second;
+            }
+            if(i+1==n){break;}
+            a = find(i, i+1, s);
+            if(p.second-p.first < a.second-a.first){
+                p.first = a.first;
+                p.second=a.second;
             }
         }
-        return s.substr(a,b-a+1);
+        return s.substr(p.first, p.second-p.first+1);
     }
 };
