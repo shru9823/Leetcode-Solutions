69ebaca1f36b37231aa5cecd9effb1910198fea8
shru9823 <Shrutikohakade9823@gmail.com>
2024-03-29 12:59:22 -0400

    Time: 64 ms (6.12%), Space: 27.5 MB (19.18%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        map<string, vector<string>> mp;
+        vector<int> vec(26, 0);
+        for(string a: strs){
+            vec.assign(26,0);
+            for(char c:a){
+                vec[c-'a']++;
+            }
+            string s="";
+            for(int i=0;i<26;i++){
+                if(vec[i] != 0){
+                    s += to_string(i) + " " + to_string(vec[i]);
+                }
+            }
+            mp[s].push_back(a);
+        }
+        vector<vector<string>> ans;
+        for(auto p:mp){
+            ans.push_back(p.second);
+        }
+        return ans;
+    }
+};
