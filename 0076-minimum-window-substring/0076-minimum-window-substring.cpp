ae6ebff2f34d58d2483562502f1ccd9bf44d895b
shru9823 <Shrutikohakade9823@gmail.com>
2024-04-01 17:20:05 -0400

    Time: 33 ms (17.37%), Space: 10.8 MB (12.83%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        map<char,int> mps, mpt;
+        int n=s.length(), len = t.length(),cnt=0, l=0,r=n-1;
+        if(n<len){return "";}
+        vector<int> ans(3);
+        for(int i=0;i<len;i++){
+            mpt[t[i]]++;
+        }
+        cnt=mpt.size();
+        for(int i=0;i<n;i++){
+            mps[s[i]]++;
+            if(mpt.find(s[i]) != mpt.end() && mpt[s[i]] == mps[s[i]]){
+                cnt--;
+            }
+            if(cnt==0){r=i;break;}
+        }
+        if(cnt!=0){return "";}
+        cout<<l<<" "<<r<<endl;
+        while(l<=r && (mpt[s[l]]<=mps[s[l]]-1)){
+            // cout<<l<<endl;
+            mps[s[l]]--;
+            l++;
+        }
+        if(r-l+1 < ans[0]){
+            ans[0]=r-l+1;
+            ans[1] = l;
+            ans[2] = r;
+        }
+        ans[0]=r-l+1;
+        ans[1] = l;
+        ans[2] = r;
+        r++;
+        while(r<n){
+            mps[s[r]]++;
+            // cout<<l<<" mpt[s[l]] "<<mpt[s[l]]<<endl;
+            while(l<=r && (mpt[s[l]]<=mps[s[l]]-1)){
+                // cout<<l<<endl;
+                mps[s[l]]--;
+                l++;
+            }
+            if(r-l+1 < ans[0]){
+                ans[0]=r-l+1;
+                ans[1] = l;
+                ans[2] = r;
+            }
+            r++;
+        }
+        return s.substr(ans[1], ans[0]);
+    }
+};
