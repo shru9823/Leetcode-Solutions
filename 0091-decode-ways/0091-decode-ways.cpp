0025889374a1ba9491f52376e25794b937fb3af3
shru9823 <Shrutikohakade9823@gmail.com>
2024-04-01 20:16:39 -0400

    Time: 5 ms (13.98%), Space: 7.6 MB (78.12%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
     int numDecodings(string s) {
-        int one=1, two=1, n=s.length();
+        
+        int n=s.length(), one=1,two=1,cur=1;
         if(s[0] == '0'){return 0;}
-        for(int i=1; i<n;i++){
-            int cur=0, num=stoi(s.substr(i-1, 2));
-            if(s[i] == '0' && (num < 10 || num > 26)){return 0;}
-            else if(s[i] != '0' && num <= 26 && num >= 10){cur += one+two;}
-            else if(s[i] == '0'){cur += two;}
-            else{cur += one;}
-            two = one;
-            one = cur;
+        char prev = s[0];
+        for(int i=1;i<n;i++){
+            cur=0;
+            string a = "";
+            a += s[i-1];
+            a+=s[i];
+            if(s[i] == '0' && prev > '2'){return 0;}
+            cout<<a<<endl;
+            if(a <= "26" && a >= "10"){
+                cur = two;
+            }
+            if(s[i] != '0'){
+                cur += one;
+            }
+            prev= s[i]
+;           two = one;
+            one =cur;
         }
-        return one;
+        return cur;
     }
 };
