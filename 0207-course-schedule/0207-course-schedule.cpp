5db942e80244f8d97af9f9e9818098884031e298
shru9823 <Shrutikohakade9823@gmail.com>
2024-04-05 22:48:34 -0400

    Time: 77 ms (5.06%), Space: 17.2 MB (71.64%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -3,17 +3,21 @@ public:
     bool find(vector<vector<int>>& vec, int parent,int cur, vector<bool>&visi){
         visi[cur]=true;
         for(int a:vec[cur]){
-            if(a==parent){return false;}
+            if(a==parent){
+                // visi[cur]=false;
+                return false;}
             if(!visi[a]){
                 if(!find(vec, parent, a, visi)){
+                    // visi[cur]=false;
                     return false;
                 }
             }
         }
+        // visi[cur]=false;
         return true;
     }
     bool canFinish(int n, vector<vector<int>>& prerequisites) {
-        vector<bool> visi(n); 
+        vector<bool> visi(n), v(n); 
         vector<vector<int>> vec(n);
         
         for(int i=0;i<prerequisites.size();i++){
@@ -22,7 +26,8 @@ public:
         for(int i=0;i<n;i++){
             if(!visi[i]){
                 if(!find(vec, i,i, visi)){return false;}
-                visi.assign(n,false);
+                // visi.assign(n,false);
+                visi=v;
             }
         }
         return true;
