a159668e7a40d20d8b49f1982b8cc166d4c5d01c
shru9823 <Shrutikohakade9823@gmail.com>
2024-04-06 23:24:22 -0400

    Time: 15 ms (42.81%), Space: 22.7 MB (31.55%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -23,9 +23,21 @@ public:
         find(root->right, cnt);
     }
     int kthSmallest(TreeNode* root, int k) {
-        int a=0;
-        kk=k;
-        find(root, a);
-        return res;
+        // int a=0;
+        // kk=k;
+        // find(root, a);
+        // return res;
+        stack<TreeNode*> st;
+        while(true){
+            while(root){
+                st.push(root);root=root->left;
+            }
+            root=st.top();st.pop();
+            if(--k == 0){
+                return root->val;
+            }
+            root = root->right;
+        }
+        return 0;
     }
 };
