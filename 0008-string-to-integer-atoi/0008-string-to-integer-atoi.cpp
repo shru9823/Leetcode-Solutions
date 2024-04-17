bbc826e57c5001da0ca8456ceb54b5e93322605b
shru9823 <Shrutikohakade9823@gmail.com>
2024-04-17 12:17:52 -0400

    Time: 4 ms (28.38%), Space: 8.3 MB (37.50%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int num=0,sign=1,n=s.length(),i=0;
+        while(i<n){
+            if(s[i] == ' '){i++;}else{break;}
+        }
+        if(s[i] == '-'){sign=-1;i++;}
+        else if(s[i] == '+'){sign=1;i++;}
+        while(i<n && s[i]=='0'){
+            i++;
+        }
+        while(i<n && s[i]>='0' && s[i] <= '9'){
+            if(num>INT_MAX/10 || (num==INT_MAX/10 && s[i]-'0'>7)){return INT_MAX;}
+            if(num<INT_MIN/10 || (num==INT_MIN/10 && s[i]-'0'>8)){return INT_MIN;}
+
+            num = num*10 + (s[i]-'0')*sign;
+            cout<<num<<endl;
+            if(sign == -1 && num>0){num = num*sign;}
+            i++;
+        }
+        return num;
+    }
+};
