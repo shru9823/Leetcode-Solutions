0148d7197dcc072389388c77877338b3a4ddbea4
shru9823 <Shrutikohakade9823@gmail.com>
2024-04-19 19:15:44 -0400

    Time: 408 ms (18.04%), Space: 93.9 MB (52.39%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -3,23 +3,16 @@ public:
     int find(vector<vector<int>>& vec, vector<int>& dp, int index){
         if(index == dp.size()){return 0;}
         if(dp[index]!=-1){return dp[index];}
-        int l=index+1,r=dp.size()-1,found=0;
+        int l=index+1,r=dp.size()-1;
+        bool found=0;
         while(l<=r){
             int mid = l+(r-l)/2;
-            while(vec[mid][0] == vec[index][1]){
-                found=true;
-                l = mid--;
-            }
+            while(vec[mid][0] == vec[index][1]){found=true;l = mid--;}
             if(found==true){break;}
-            if(vec[mid][0] > vec[index][1]){
-                r=mid-1;
-            }else{
-                l = mid+1;
-            }
+            if(vec[mid][0] > vec[index][1]){r=mid-1;}
+            else{l = mid+1;}
         }
-        // cout<<l<<endl;
-        dp[index] = max(find(vec,dp,index+1), vec[index][2] + (l!=index?find(vec,dp,l):0));
-        return dp[index];
+        return dp[index] = max(find(vec,dp,index+1), vec[index][2] + (l!=index?find(vec,dp,l):0));
     }
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
         int n=profit.size();
