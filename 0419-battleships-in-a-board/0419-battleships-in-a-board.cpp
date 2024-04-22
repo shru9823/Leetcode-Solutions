3834ed8934bcd64ef8361f79d9d854f9e7ba2ebd
shru9823 <Shrutikohakade9823@gmail.com>
2024-04-22 13:05:27 -0400

    Time: 6 ms (56.20%), Space: 11.7 MB (17.23%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
@@ -5,18 +5,8 @@ public:
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(board[i][j] != 'X'){continue;}
-                // if(i-1>=0 && j-1>=0){
-                    if((i-1>=0 && board[i-1][j]=='X') || (j-1>=0 && board[i][j-1] == 'X')){continue;}
-                    else{ans++;}
-                // }else if(i-1>=0){
-                //     if(board[i-1][j]=='X'){continue;}
-                //     else{ans++;}
-                // }else if(j-1 >= 0){
-                //     if(board[i][j-1]=='X'){continue;}
-                //     else{ans++;}
-                // }else{
-                //     ans++;
-                // }
+                if((i-1>=0 && board[i-1][j]=='X') || (j-1>=0 && board[i][j-1] == 'X')){continue;}
+                else{ans++;}
             }
         }
         return ans;
